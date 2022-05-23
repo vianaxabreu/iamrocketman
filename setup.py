b9f2db3a5ec264af03a8bf3813baddc05c5a3c1e
Alexsandro Viana de Abreu <viana.abreu@outlook.com>
2022-05-23 13:00:21 +0200

    creat a script to play songh

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='iamrocketman',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/iamrocketman-run'],
+      scripts=['scripts/iamrocketman-run', 'playsong-run'],
       zip_safe=False)
